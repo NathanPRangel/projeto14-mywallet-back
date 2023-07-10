00620eb2d8aaabd45e2207b47176efa8ebb823f7
NathanPRangel <nathanpk20@gmail.com>
2023-07-10 16:20:45 -0300

    correção2

--- src/db.js
+++ src/db.js
@@ -2,9 +2,11 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.DATABASE_URL);
+const url = process.env.DATABASE_URL;
+
+const mongoClient = new MongoClient(url);
 
 await mongoClient.connect();
-const db = mongoClient.db()
+const db = mongoClient.db();
 
-export default db; 
+export default db;
